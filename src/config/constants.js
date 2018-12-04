d71a626763800dae8eca520f2c2c1119698bc450
Alfie Freeman <runticle@gmail.com>
2018-12-04 17:38:37 +0000

    adds message about shop

--- src/config/constants.js
+++ src/config/constants.js
@@ -6,5 +6,6 @@ export const MESSAGES = [
   "Use 'e' to pick up eggs and 'q' to talk to people!",
   "You found an egg!",
   "walk through the trees here mate!",
-  "hello! I'm old man"
+  "hello! I'm old man",
+  "this is the general store, buy some shit!"
 ]
